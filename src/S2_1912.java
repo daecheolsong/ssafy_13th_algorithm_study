c83d950074d1c8e864613654bc40249267a17f99
daeyeol-yang <yang980310@naver.com>
2025-01-20 12:02:32 +0900

    1912

--- src/S2_1912.java
+++ src/S2_1912.java
@@ -1,4 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class S2_1912 {
-
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		int sum =0;
+		
+		int max = Integer.MIN_VALUE;
+		for(int i =0 ; i<N ; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			sum += num;
+			
+			
+			max = Math.max(max, sum);
+			
+			if(sum<0) {
+				sum =0;
+			}
+			
+		}
+		
+		System.out.println(max);
+		
+		
+		
+	}
 }
