7d815efd680a30a44fbd106e807bdbd50a023dd3
shigwon <96384712+shigwon@users.noreply.github.com>
2025-01-24 08:30:55 +0900

    Update S5_11650.java

--- S5_11650.java
+++ S5_11650.java
@@ -10,6 +10,17 @@ public class S5_11650 {
 	        for (int i = 0; i < n; i++) {
 	            arr[i][0]  = sc.nextInt();
 	            arr[i][1] = sc.nextInt();
+
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
 	        
 	        
 	        }
