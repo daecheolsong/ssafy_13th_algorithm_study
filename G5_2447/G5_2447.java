4004c09a160c63800d6d95a34cbad00cd37d077d
daecheolsong <eocjf8905@naver.com>
2025-02-06 16:52:06 +0900

    soooolve

--- G5_2447/G5_2447.java
+++ G5_2447/G5_2447.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class S5_2447 {
+public class G5_2447 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
