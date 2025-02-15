7b2ede732196fc11a6edce10c94200e195256890
shigwon <96384712+shigwon@users.noreply.github.com>
2025-02-15 23:44:11 +0900

    Add files via upload

--- S5_11650.java
+++ S5_11650.java
@@ -1,30 +1,38 @@
-package algo;
-
-import java.util.Scanner;
+package algorithm.class2;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class S5_11650 {
-	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        int n = sc.nextInt();
-	        int [][] arr = new int[n][2]; 
-	        for (int i = 0; i < n; i++) {
-	            arr[i][0]  = sc.nextInt();
-	            arr[i][1] = sc.nextInt();
 
-		Arrays.sort(arr, (e1, e2) -> {
-			if(e1[0] == e2[0]) {
-				return e1[1] - e2[1];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[N][2];
+		
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (o1, o2) -> {
+			if(o1[0] == o2[0]) {
+				return o1[1] - o2[1];
 			} else {
-				return e1[0] - e2[0];
+				return o1[0] - o2[0];
 			}
 		});
 		
-		for(int i = 0; i < N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
-	        
-	        
-	        }
-	    }
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i< N ; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+		}
+		System.out.println(sb);
+	}
+ 
+ 
 }
-
-
