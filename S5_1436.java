9ffd02cf01a9ee8192760b46101abca39936edd5
shigwon <96384712+shigwon@users.noreply.github.com>
2025-02-18 23:19:24 +0900

    Add files via upload

---
+++ S5_1436.java
@@ -0,0 +1,24 @@
+package BOJ.class2;
+
+import java.io.*;
+
+public class S5_1436 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int num = 666;
+		int count = 1;
+
+		while (count != N) {
+			num++;
+			if (String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+
+	}
+
+}
