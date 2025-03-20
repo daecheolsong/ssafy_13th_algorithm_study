5eff67fe9a3abb69d4ed7316113ea75294e9df22
shigwon <96384712+shigwon@users.noreply.github.com>
2025-03-21 00:45:15 +0900

    Add files via upload

---
+++ S3_15655.java
@@ -0,0 +1,44 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class S3_15655 {
+	static int N, M;
+	static int[] input, numbers;
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		numbers = new int[M];
+		input = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		combi(0,0);
+	}
+
+	private static void combi(int cnt, int start) {
+		if(cnt==M) {
+			for (int i = 0; i < M; i++) {
+				System.out.print(numbers[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = start; i < input.length; i++) {
+			numbers[cnt] = input[i];
+			combi(cnt+1, i+1);
+		}
+	}
+
+}
