b41c4500b6ac595154d5ea82c2dbfa09078c069c
shigwon <96384712+shigwon@users.noreply.github.com>
2025-04-26 00:27:35 +0900

    Create G4_1715.java

---
+++ G4_1715.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			int data = Integer.parseInt(br.readLine());
+			pq.add(data);
+		}
+		
+		int data1 = 0;
+		int data2 = 0;
+		int sum = 0;
+		while(pq.size() != 1) {
+			data1 = pq.remove();
+			data2 = pq.remove();
+			sum += data1+data2;
+			pq.add(data1+data2);
+		}
+		System.out.println(sum);
+
+	}
+
+}
